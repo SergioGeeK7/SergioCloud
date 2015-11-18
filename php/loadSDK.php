47554889e06cf9a233a6443c1eed1e9e3cdf6d27
Sergio A <SergioGeeK7@gmail.com>
2015-11-18 16:16:29 -0500

    Fixed config because php 5.2 does not support direct array
    modify method()[] because is not supported by php 5.2

--- php/loadSDK.php
+++ php/loadSDK.php
@@ -3,6 +3,7 @@ require_once "dropbox-sdk/Dropbox/autoload.php";
 require_once "Helps.php";
 use \Dropbox as dbx;
 dbx\AppInfo::loadFromJsonFile("../privateConfig.json");
-$dbxClient = new dbx\Client(readConfig("../privateConfig.json")["token"], "PHP-Example/1.0");
+$config = readConfig("../privateConfig.json");
+$dbxClient = new dbx\Client($config["token"], "PHP-Example/1.0");
 $addFile = dbx\WriteMode::add();
 ?>
